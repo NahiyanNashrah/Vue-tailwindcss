0a5ad5f3367c5dfa9fb7e848d215a558bbcb64a9
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-03-06 12:40:54 +0600

    edit style

--- src/components/deliveryDetails.vue
+++ src/components/deliveryDetails.vue
@@ -1,105 +1,95 @@
 <template>
-  <div>
-    <div class="container md:w-8/12 mt-1">
-      <div class="bg-white shadow-gray px-4 py-4 flex mt-1">
-        <div class="w-1/2">
-          <h1 class="text-gray-500">Consignment Id</h1>
-        </div>
-        <div class="w-1/2 flex flex-col items-end">
-          <h2 class="text-black font-bold">{{ order.id }}</h2>
+  <div class="container">
+    <div class="home">
+      <span class="home-element">Home</span>
+      <span>&#62;</span>
+      <span class="home-element">Tracking</span>
+      <span>&#62;</span>
+      <span class="home-element">Delivery Details</span>
+    </div>
+    <div class="consignment flex flex-row justify-between">
+      <span class="consignment-title">Consignment Id</span>
+      <div>
+        <span class="consignment-id">{{ order.id }}</span>
+        <img class="consignment-img" src="../assets/resource/ic_copy.svg" />
+      </div>
+    </div>
+    <div class="shipping flex flex-row justify-between">
+      <div class="shipping-title w-1/4">
+        <p>Shipping Info</p>
+      </div>
+      <div class="shipping-info w-1/2">
+        <p class="shipping-contact">{{ user.phone }}</p>
+        <div class="shipping-address">
+          <p>{{user.name}}</p>
+          <p>{{ user.address}}</p>
         </div>
       </div>
-      <div
-        class="bg-white shadow-gray px-4 py-4 mt-1 flex flex-row justify-between"
-      >
-        <div class="w-1/4">
-          <h1 class="text-gray-500">Shipping Info</h1>
+    </div>
+    <div class="timeline">
+      <p class="timeline-title">Timeline</p>
+      <div class="timeline-img"></div>
+      <div class="timeline-details flex-col">
+        <div class="item">
+          <p class="title">Your order has been picked-up!</p>
+          <p class="time">March 1,2020,10:00</p>
         </div>
-        <div class="w-1/2 flex flex-col items-end">
-          <h2 class="text-black font-bold">{{ user.phone }}</h2>
-          <p>{{ user.name }}</p>
-          <p>{{ user.address }}</p>
+        <div class="item">
+          <p class="title">Our delivery man is on his way to pick up your order.</p>
+          <p class="time">March 1,2020,10:00</p>
         </div>
-      </div>
-
-      <div class="bg-white shadow-gray py-4 px-4 mt-1 flex flex-col">
-        <h1 class="text-black font-bold mb-2">Timeline</h1>
-        <div class="flex flex-row">
-          <hr class="w-1 h-12 bg-red-500 rounded mr-2" />
-          <div class="mb-2">
-            <h3 class="font-medium">Your order has been picked-up!</h3>
-            <p class="text-gray-400">March 1,2020,10:00</p>
-          </div>
+        <div class="item">
+          <p class="title">Your Order has been confirmed!</p>
+          <p class="time">March 1,2020,10:00</p>
         </div>
-        <div class="flex flex-row">
-          <hr class="w-1 h-12 bg-blue-500 rounded mr-2" />
-          <div class="mb-2">
-            <h3 class="font-medium">
-              Our delivery man is on his way to pick up your order.
-            </h3>
-            <p class="text-gray-400">March 1,2020,10:00</p>
-          </div>
+        <div class="item">
+          <p class="title">Your Order has been confirmed!</p>
+          <p class="time">March 1,2020,10:00</p>
         </div>
-        <div class="flex flex-row">
-          <hr class="w-1 h-12 bg-green-500 rounded mr-2" />
-          <div class="mb-2">
-            <h3 class="font-medium">Your Order has been confirmed!</h3>
-            <p class="text-gray-400">March 1,2020,10:00</p>
-          </div>
+        <div class="item">
+          <p class="title">Your Order has been confirmed!</p>
+          <p class="time">March 1,2020,10:00</p>
         </div>
       </div>
-      <div class="bg-white shadow-gray py-4 px-4 mt-1 flex flex-row">
-        <div class="w-1/2">
-          <h1 class="text-black font-bold">Product Description</h1>
-          <div class="flex flex-col items-start font-normal mt-2">
-            <p>01 Black Tshirt</p>
-            <p>02 Knee Guard</p>
-          </div>
-        </div>
-        <div class="w-1/2">
-          <h1 class="text-black font-bold">Receipt</h1>
-          <div class="w-full flex flex-col mt-2">
-            <div class="flex font-normal mb-2">
-              <p class="w-1/2 flex flex-col items-start">Product price</p>
-              <p class="w-1/2 flex flex-col items-end">&#2547; 700</p>
-            </div>
-            <div class="w-full flex font-normal mb-2">
-              <p class="w-1/2 flex flex-col items-start">Delivery Charge</p>
-              <p class="w-1/2 flex flex-col items-end">&#2547; 60</p>
-            </div>
-            <hr />
-            <div class="w-full flex font-medium mb-2">
-              <p class="w-1/2 flex flex-col items-start">Total</p>
-              <p class="w-1/2 flex flex-col items-end">&#2547; 760</p>
-            </div>
-          </div>
+      <div class="load-more">
+        <img src="../assets/resource/ic_down_arrow.svg" class="load-more-icon" />
+        <span class="load-more-title">Load more</span>
+      </div>
+    </div>
+    <div class="product flex flex-row justify-between">
+      <div class="w-1/2 product-description">
+        <p class="product-title">Product Description</p>
+        <div class="product-list">
+          <p class="item">01 Black Tshirt</p>
+          <p class="item">02 Knee Guard</p>
         </div>
       </div>
-
-      <div class="bg-white shadow-gray py-4 px-4 mt-1">
-        <h1 class="text-black font-bold">Seller Info</h1>
-        <div class="flex flex-row">
-          <div class="w-20">
-            <img src="../assets/resource/logo.png" class="object-cover h-20" />
+      <div class="w-1/2 receipt">
+        <p class="receipt-title">Receipt</p>
+        <div class="receipt-details">
+          <div class="element flex flex-row justify-between">
+            <p class="element-name">Product price</p>
+            <p>&#2547; 700</p>
           </div>
-          <div class="w-6/9">
-            <h2 class="capitalize text-red-500">{{ merchant.name }}</h2>
-            <p class="capitalize text-black">{{ merchant.description }}</p>
+          <div class="element flex flex-row justify-between">
+            <p class="element-name">Delivery Charge</p>
+            <p>&#2547; 60</p>
+          </div>
+          <p class="element dotted-separator"></p>
+          <div class="element flex flex-row justify-between">
+            <p class="total">Total</p>
+            <p class="total-price">&#2547; 600</p>
           </div>
         </div>
       </div>
-      <div class="bg-white shadow-gray py-4 px-4 mt-1">
-        <div class="flex flex-row">
-          <button
-            class="w-1/2 bg-red-600 hover:bg-red-700 text-white uppercase font-bold py-2 px-2 mr-3 rounded"
-          >
-            Call Support
-          </button>
-          <button
-            class="w-1/2 bg-gray-500 hover:bg-gray-700 text-black uppercase font-bold py-2 px-2 rounded"
-          >
-            Chat Bot
-          </button>
+    </div>
+    <div class="merchant">
+      <p class="merchant-title">Seller Info</p>
+      <div class="merchant-info flex flex-row items-center">
+        <img src="../assets/resource/logo.png" class="merchant-logo" />
+        <div class="merchant-details">
+          <p class="merchant-name">{{merchant.name}}</p>
+          <p class="merchant-description">{{ merchant.description }}</p>
         </div>
       </div>
     </div>
